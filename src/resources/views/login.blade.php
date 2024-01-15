50fc28314781c8b70726e94dcf0afd3c168c42bc
hosomitadasi <hosomitadasi@gmail.com>
2024-01-16 00:03:46 +0900

    ログイン画面の作成

---
+++ src/resources/views/login.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
+
+@section('content')
+<div class="login__content">
+    <form action="/html/data.html" method="get">
+        <div class="login__form">
+            <div class="login__form-header">
+                <div class="login__form-logo">
+                    <p>ログイン</p>
+                </div>
+            </div>
+
+            <div class="login__form-input">
+                <input type="text" name="email" placeholder="メールアドレス">
+            </div>
+
+            <div class="login__form-input">
+                <input type="text" name="password" placeholder="パスワード">
+            </div>
+
+            <div class="login__button">
+                <button class="login__button-submit" type="submit">ログイン</button>
+            </div>
+
+        </div>
+    </form>
+
+    <div class="register__form">
+        <div class="register__logo">
+            <p>アカウントをお持ちでない方はこちらから</p>
+        </div>
+        <div class="register__link">
+            <a class="register__link-logo" href="register.html">会員登録</a>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ src/resources/views/register.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('content')
+<div class="register__form-content">
+    <div class="register__form-logo">
+        <h2>会員登録</h2>
+    </div>
+
+    <form>
+        <div class="register__form-group">
+            <div class="register__input">
+                <input type="taxt" name="name" placeholder="名前" />
+            </div>
+
+            <div class="register__input">
+                <input type="taxt" name="email" placeholder="メールアドレス" />
+            </div>
+
+            <div class="register__input">
+                <input type="taxt" name="password" placeholder="パスワード" />
+            </div>
+
+            <div class="register__input">
+                <input type="taxt" name="confirmation--password" placeholder="確認用パスワード" />
+            </div>
+
+            <div class="register__button">
+                <button class="register__button-submit" type="submit">会員登録</button>
+            </div>
+        </div>
+    </form>
+
+    <div class="login__form">
+        <div class="login__logo">
+            <p>アカウントをお持ちの方はこちらから</p>
+        </div>
+        <div class="login__link">
+            <a class="login" href="login.html">ログイン</a>
+        </div>
+    </div>
+</div>
+@endsection
